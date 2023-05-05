bff822f0604719437e0f6c6c0dece1e2eccddb3e
Kostiantyn Smyrnov <kostysh@gmail.com>
2023-05-05 20:56:24 +0200

    refactor: 💡 Refactored setup and utils

--- test/contracts/setup.ts
+++ test/contracts/setup.ts
@@ -1,6 +1,7 @@
 import { MockERC20Dec18, MockERC20Dec18Permit, Market } from '../../typechain';
 import { ethers, deployments, getNamedAccounts } from 'hardhat';
 import { BigNumber, Contract, VoidSigner } from 'ethers';
+import { structEqual, createSupplierId } from './utils';
 
 export interface Contracts {
   erc20: MockERC20Dec18;
@@ -18,6 +19,9 @@ export type Users = Record<string, User>;
 export const eip712name = 'Market';
 export const eip712version = '1';
 export const minDeposit = BigNumber.from('1000000000000000000000');
+export const claimPeriod = BigNumber.from('60');
+export const protocolFee = BigNumber.from('1');
+export const retailerFee = BigNumber.from('1');
 
 export const setupUser = async (
   address: string,
@@ -64,3 +68,38 @@ export const setup = deployments.createFixture(async () => {
     users,
   };
 });
+
+export const registerSupplier = async (
+  market: Market,
+  supplierSalt: string,
+  supplierOwner: User,
+  supplierSigner: User,
+  lif?: MockERC20Dec18Permit,
+  enable = true,
+) => {
+  await market.register(supplierSalt, supplierSigner.address);
+  const supplierId = createSupplierId(supplierOwner.address, supplierSalt);
+  structEqual(await market.suppliers(supplierId), {
+    id: supplierId,
+    owner: supplierOwner.address,
+    enabled: false,
+    signer: supplierSigner.address,
+  });
+  if (lif) {
+    await lif.approve(market.address, minDeposit);
+    await market['addDeposit(bytes32,uint256)'](supplierId, minDeposit);
+  }
+  if (enable) {
+    await market.toggleSupplier(supplierId);
+  }
+};
+
+export const calcFees = (value: BigNumber) => {
+  const percentage = (val: BigNumber, perc: BigNumber) =>
+    val.mul(1000).mul(perc).div(100).div(1000);
+  const protocolFeeValue = percentage(value, protocolFee);
+  const retailerFeeValue = percentage(value, retailerFee);
+  const supplierValue = value.sub(protocolFee).sub(retailerFee);
+
+  return { protocolFeeValue, retailerFeeValue, supplierValue };
+};
