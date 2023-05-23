318a9d8658b93c9416fe8cc665e03472e0e3faa1
Kostiantyn Smyrnov <kostysh@gmail.com>
2023-05-23 21:10:44 +0200

    feat: 🎸 Added tasks: entity, encodeBytes32String;

--- tasks/index.ts
+++ tasks/index.ts
@@ -1,14 +1,16 @@
+/* eslint-disable @typescript-eslint/require-await */
 /* eslint-disable @typescript-eslint/no-unsafe-member-access */
 /* eslint-disable @typescript-eslint/no-unsafe-assignment */
 import { task } from "hardhat/config";
 import { utils, Wallet } from "ethers";
+import { randomId, createSupplierId } from "../utils";
 
 task("debug", "Transaction debug")
   .addParam("tx", "Transaction hash")
   .setAction(async (args, hre) => {
     const txHash = args.tx;
     const trace = await hre.network.provider.send(
-      "eth_getTransactionReceipt", // debug_traceTransaction
+      "debug_traceTransaction", // debug_traceTransaction
       [
         txHash,
         // {
@@ -21,12 +23,38 @@ task("debug", "Transaction debug")
     console.log(JSON.stringify(trace, null, 2));
   });
 
-task("wallet", "Generate random wallet")
-  // eslint-disable-next-line @typescript-eslint/require-await
-  .setAction(async () => {
-    const mnemonic = utils.entropyToMnemonic(utils.randomBytes(32));
-    console.log(`Mnemonic: ${mnemonic}`);
-    const wallet = Wallet.fromMnemonic(mnemonic);
-    console.log("Account address:", wallet.address);
-    console.log("PK:", wallet.privateKey);
+task("wallet", "Generate random wallet").setAction(async () => {
+  const mnemonic = utils.entropyToMnemonic(utils.randomBytes(32));
+  console.log(`Mnemonic: ${mnemonic}`);
+  const wallet = Wallet.fromMnemonic(mnemonic);
+  console.log("Account address:", wallet.address);
+  console.log("PK:", wallet.privateKey);
+});
+
+task("entity").setAction(async () => {
+  const ownerMnemonic = utils.entropyToMnemonic(utils.randomBytes(32));
+  console.log(`Owner mnemonic: ${ownerMnemonic}`);
+  const ownerWallet = Wallet.fromMnemonic(ownerMnemonic);
+  console.log("Owner account address:", ownerWallet.address);
+  console.log("Owner PK:", ownerWallet.privateKey, "\n");
+
+  const signerMnemonic = utils.entropyToMnemonic(utils.randomBytes(32));
+  console.log(`Signer mnemonic: ${signerMnemonic}`);
+  const signerWallet = Wallet.fromMnemonic(signerMnemonic);
+  console.log("Signer account address:", signerWallet.address);
+  console.log("Signer PK:", signerWallet.privateKey, "\n");
+
+  const salt = randomId();
+  console.log(`Entity Id salt: ${salt}`);
+  console.log(`Entity Id:`, createSupplierId(ownerWallet.address, salt));
+});
+
+task("encodeBytes32String")
+  .addParam("string", "String to encode")
+  .setAction(async (args) => {
+    console.log("String:", args.string);
+    console.log(
+      "Bytes32String:",
+      utils.formatBytes32String(args.string as string)
+    );
   });
